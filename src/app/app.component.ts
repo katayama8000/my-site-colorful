2c6670ffa4f12a6e016b9c6b4428db85b9efb0d4
katayama <tattu.0310@gmail.com>
2022-09-08 12:12:31 +0900

    💄 add/title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [Title], // ［1］Titleサービスを登録
 })
 export class AppComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private title: Title) {
+    // ［2］タイトルを設定
+    this.title.setTitle('My-Site');
+  }
 }
